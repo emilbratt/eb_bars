c046bb41fd4c6847bfbb1e80b05ded3557f16c7f
emilbratt <emilbratt@gmail.com>
2025-03-11 20:43:53 +0100

    Bump version 0.4.2 -> 0.4.3

--- src/lib.rs
+++ src/lib.rs
@@ -85,7 +85,7 @@ mod svg;
 
 type Percentage = i16;
 
-const VERSION: &str = "0.4.2";
+const VERSION: &str = "0.4.3";
 const REPOSITORY: &str = "https://github.com/emilbratt/eb_bars";
 
 const DEFAULT_BAR_COLOR: &str = "rgb(112, 153, 182)";
